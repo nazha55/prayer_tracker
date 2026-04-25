1da7fe347bf2e26e2ff583878579413c535e86a1
NazhaNoufal <nazhanoufal55@gmail.com>
2026-04-25 17:11:41 +0530

    add requirements

--- db/connection.py
+++ db/connection.py
@@ -7,9 +7,6 @@ load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        os.getenv("DATABASE_URL"),
         cursor_factory=RealDictCursor
     )
